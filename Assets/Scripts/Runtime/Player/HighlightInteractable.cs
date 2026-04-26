cb2a10b23d2bce5ba42d62554ee531467473ef08
Kristoffer Jakobsson <kristoffer.tatel.jakobsson@gmail.com>
2026-04-26 14:23:09 +0200

    create highlight controller

---
+++ Assets/Scripts/Runtime/Player/HighlightInteractable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using JamJam.Runtime.Utility.Extensions;
+using UnityEngine;
+
+namespace JamJam.Runtime.Player {
+    public class HighlightInteractable : MonoBehaviour {
+        public GameObject[] outlines;
+        
+        public bool LookingAt { get; set; }
+
+        private void LateUpdate() {
+            outlines.ForEach(x => x.SetActive(LookingAt));
+            LookingAt = false;
+        }
+    }
+}
